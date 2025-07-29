90e8f83b0e217b8ae9dbbf745a344795c654a94f
TofiTurtle <juan.rodriguez.lozano@correounivalle.edu.co>
2025-07-29 01:40:46 -0500

    Basic PlayerSetUp finished, working fine

--- src/main/java/org/example/eiscuno/controller/PlayerSetUpController.java
+++ src/main/java/org/example/eiscuno/controller/PlayerSetUpController.java
@@ -2,14 +2,72 @@ package org.example.eiscuno.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import org.example.eiscuno.view.GameUnoStage;
 import org.example.eiscuno.view.PlayerSetUpStage;
 import org.example.eiscuno.view.StartUnoView;
 
 import java.io.IOException;
+import java.util.List;
 
 public class PlayerSetUpController {
+    @FXML
+    private ImageView imageView;
+    @FXML
+    private TextField textField;
+    @FXML
+    private Label emptyNameLabel;
+    private List<Image> images;
+    private int currentIndex = 2;
 
+    @FXML
+    public void initialize() {
+        textField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.trim().isEmpty()) {
+                emptyNameLabel.setVisible(false); // mira si esta vacio para mostrar el mensaje de trin
+            }
+        });
+
+        images = List.of(
+                new Image(getClass().getResourceAsStream("/org/example/eiscuno/cards-uno/skip_blue.png")),
+                new Image(getClass().getResourceAsStream("/org/example/eiscuno/cards-uno/skip_green.png")),
+                new Image(getClass().getResourceAsStream("/org/example/eiscuno/cards-uno/skip_yellow.png")),
+                new Image(getClass().getResourceAsStream("/org/example/eiscuno/cards-uno/skip_red.png"))
+        );
+        imageView.setImage(images.get(currentIndex));
+
+    }
+
+    @FXML
+    private void nextImage() {
+        if (currentIndex < images.size() - 1) {
+            currentIndex++;
+            imageView.setImage(images.get(currentIndex));
+        } else {
+            currentIndex = 0;
+            imageView.setImage(images.get(currentIndex));
+        }
+    }
+
+    @FXML
+    private void previousImage() {
+        if (currentIndex > 0) {
+            currentIndex--;
+            imageView.setImage(images.get(currentIndex));
+
+        }else {
+            currentIndex = images.size() - 1;
+            imageView.setImage(images.get(currentIndex));
+        }
+    }
+
+
+
+
+    //metodos existentes de botones iniciales
     @FXML
     void goBackToStart(ActionEvent event) throws IOException {
         StartUnoView.getInstance();
@@ -20,6 +78,10 @@ public class PlayerSetUpController {
         GameUnoStage.getInstance();
         PlayerSetUpStage.deleteInstance();
     }
+
+
+
+
 
 
 }
