f34a2e68382493f40bc833bb85709530083710d6
Felipe Castro <andr3sf3lipecastro@gmail.com>
2025-07-30 08:01:39 -0500

    Generic commit, pull

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,15 @@ module org.example.eiscuno {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires javafx.graphics;
 
+    // Si usas clases de javafx.base, agrega también:
+    requires javafx.base;
 
+    exports org.example.eiscuno;
     opens org.example.eiscuno to javafx.fxml;
     opens org.example.eiscuno.controller to javafx.fxml;
-    exports org.example.eiscuno;
+
 }
+
+
